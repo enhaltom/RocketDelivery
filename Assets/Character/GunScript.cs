3c86838327484e7d5a94a41d3151c6348c8b05e6
Darrin Cation <dc3g@mtmail.mtsu.edu>
2016-10-25 18:13:15 -0500

    Darrin Cation First Alpha PUsh

---
+++ Assets/Character/GunScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour {
+    public float rotOffest = 90;
+
+	// Update is called once per frame
+	void Update () {
+        //Positon the player mouse as gun
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize(); // Normailize the vector. Meaning the vector will be equal to 1
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //Find the angle in degrees 
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotOffest);
+
+    }
+}
---
+++ Assets/Character/Shooting.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+    public float fireRate = 0;
+    public float Damage = 1;
+    public LayerMask notToHit;
+    public Transform BulletTrailPrefab;
+    public Transform muzzleFlash;
+    float timeToSpawnEffect;
+    float effectSpawmRate = 10;
+    float timeToFire = 0;
+
+    
+    Transform firePoint;
+
+    void Awake()
+    {
+        firePoint = transform.FindChild("Firepoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("Why you know have a fire point");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if (fireRate == 0)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+	}
+    void Shoot ()
+    {
+        // Debug.Log("Shoot");
+        Vector2 mousePostion = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePostion-firePointPosition, 100, notToHit);
+        if(Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawmRate;
+        }
+        Debug.DrawLine(firePointPosition, (mousePostion - firePointPosition) * 100, Color.cyan);
+        if (hit.collider != null)
+        {
+            Debug.Log("I hit something");
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
+
+        }
+
+    }
+    void Effect ()
+    {
+        Instantiate(BulletTrailPrefab,firePoint.position, firePoint.rotation);
+        Transform clone = (Transform)Instantiate(muzzleFlash, firePoint.position, firePoint.rotation);
+        clone.parent = firePoint;
+        float size = Random.Range(0.06f, 0.09f);
+        clone.localScale = new Vector3(size, size, size);
+        Destroy(clone.gameObject, 0.03f);
+
+
+    }
+}
