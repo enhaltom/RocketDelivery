7cd73674f7425fe7bc263a0d3d59514408b9eba7
Darrin Cation <dc3g@mtmail.mtsu.edu>
2016-11-03 01:25:54 -0500

    Darrin

--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -116,5 +116,19 @@ namespace UnityStandardAssets._2D
             theScale.x *= -1;
             playerGraphics.localScale = theScale;
         }
+
+		void OnCollisionEnter2d(Collision2D other)
+		{
+			if (other.transform.tag == "MovingPlatform") {
+				transform.parent = other.transform;
+			}
+		}
+		void OnCollisionEixt2d(Collision2D other)
+		{
+			if (other.transform.tag == "MovingPlatform") {
+				transform.parent = null;
+			}
+		}
     }
+
 }
