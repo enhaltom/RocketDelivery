56899f2755bf9eb55b7a08a2997ba9d3c6a5deb8
Muhammad Abed <mja3u@mtmail.mtsu.edu>
2016-11-08 18:26:10 -0600

    Basic enemy
    Added a new scene called enemyAlpha. Got basic drone into the scene and
    added a script that lets enemy chase player when they're in range.

---
+++ Assets/Character/jetpack.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jetpack : MonoBehaviour {
+
+	CharacterController cc;
+	public float jetpackForce = 75.0f;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		cc = (CharacterController) GetComponent<CharacterController> ();
+		rb = (Rigidbody2D)GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		/*
+		 if(Input.GetKey(KeyCode.LeftShift)){
+			Vector3 velocity = new Vector3 (0, 10, 0);
+
+		}
+		*/
+	}
+
+	void FixedUpdate () 
+	{
+		bool jetpackActive = Input.GetKey(KeyCode.LeftShift);
+
+		if (jetpackActive)
+		{
+			rb.AddForce (new Vector2 (0, jetpackForce));
+		}
+	}
+}
---
+++ Assets/Scripts/enemyMoveDroneCrasher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets._2D;
+
+public class enemyMoveDroneCrasher : MonoBehaviour {
+
+	private PlatformerCharacter2D thePlayer;
+
+	public float moveSpeed;
+
+	public float playerRange;
+
+	public LayerMask playerLayer;
+
+	public bool playerInRange;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = FindObjectOfType<PlatformerCharacter2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerInRange = Physics2D.OverlapCircle (transform.position, playerRange, playerLayer);
+		if (playerInRange) 
+			transform.position = Vector3.MoveTowards (transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
+	
+	}
+
+	void OnDrawGizmosSelected () {
+		Gizmos.DrawWireSphere (transform.position, playerRange);
+	}
+}
